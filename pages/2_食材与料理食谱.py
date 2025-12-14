92a3cb8cbb8addc544065e2ec34fd61f19c40073
Eeezhi <hyzsgtyg@live.com>
2025-12-15 02:02:12 +0900

    简体化食材与料理食谱页面 增加了宝可梦信息 增加了食谱信息

--- pages/2_食材与料理食谱.py
+++ pages/2_食材与料理食谱.py
@@ -15,8 +15,8 @@ from pages.util.util import (
 )
 
 st.set_page_config(page_title='Pokemon Sleep App', layout="wide")
-st.title('Pokemon Sleep 食譜')
-st.caption('利用自己現有的食材篩選能做出哪些食譜料理')
+st.title('Pokemon Sleep 食谱')
+st.caption('利用现在有的食材，查询可以料理的食谱')
 
 RECIPE_TRANSFORMED = os.path.join(database_path, 'transformed/recipe_transformed.csv')
 df = pd.read_csv(RECIPE_TRANSFORMED, index_col=0)
@@ -27,22 +27,21 @@ with ingredient_col:
     have_ingredients = st.multiselect(
         '食材', 
         ingredient_unique_list,
-        placeholder='請選擇食材（可多選）'
+        placeholder='请选择食材（可多选）'
     )
 with match_mode_col:
     # match_mode = st.checkbox('任一食材符合', False)
-    match_mode = st.radio('篩選方式', ['所有食材符合', '任一食材符合'], 0)
-
+    match_mode = st.radio('筛选方式', ['所有食材符合', '任一食材符合'], 0)
 category_col, recipe_col = st.columns(2)
 with category_col:
-    category = st.selectbox('食譜分類', category_list)
+    category = st.selectbox('食谱分类', category_list)
 with recipe_col:
-    recipe = st.selectbox('食譜名稱', all_recipe_dict.get(category, all_recipe_dict['全部']))
+    recipe = st.selectbox('食谱名称', all_recipe_dict.get(category, all_recipe_dict['全部']))
 
 st.divider()
 
 ingredients_str_list = ', '.join(have_ingredients) if have_ingredients else '全部'
-st.write(f"目前選擇的食材:")
+st.write(f"目前选择的食材:")
 st.info(f"{ingredients_str_list}")
 
 can_cook = get_can_cook(df, have_ingredients, match_mode)
@@ -57,7 +56,7 @@ def color_ingredients(val):
     if val is not np.nan and any(i in val for i in have_ingredients):
         return f'background-color: {color}'
 
-st.write(f"可料理食譜:")
-can_cook_filtered = can_cook_filtered[show_cols].set_index('食譜').T
+st.write(f"可料理食谱:")
+can_cook_filtered = can_cook_filtered[show_cols].set_index('食谱').T
 can_cook_filtered = can_cook_filtered.fillna('')
 st.dataframe(can_cook_filtered.style.applymap(color_ingredients))
